9c1a8113bd581fe7f06a85bd002396355d9a7609
Roman Volodin <roman.volodin@gmail.com>
2023-06-14 13:05:52 +0300

    Add variables

---
+++ 01-js/02-first-steps/04-variables.js
@@ -0,0 +1,21 @@
+"use strict"
+
+let message = 'Kolbasa';
+let $ = 'dollar';  // Не просмотреть в консоли Файрфокса. На $ завязана какая-то встроенная функция
+let _ = 'underscore';
+
+const obj = {};
+obj.age = 12;  // Работает
+
+const str = "Hello";
+// str = "Hi";  // Не работает
+
+
+// Задачи
+let admin;
+let name;
+
+name = 'John';
+admin = name;
+
+alert(admin);
